7b94e6eb88b428a93451a0b32b0bedd100552b54
CarlosGabriel-prog <carlosmenezes.dev@gmail.com>
2026-01-08 19:29:45 +0000

    Implementação completa de pib per capita e densidade populacional/mk²

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -50,17 +50,6 @@ int main() {
   printf("Quantos pontos turísticos a cidade possui: ");
   scanf("%d", &pontosTuristicos1);
 
-  // Área para calculo de dados
-
-  pib1 = pib1 * 1000000000;//not ok
-  pib2 = pib2 * 1000000000;//not ok
-
-  densidadePopulacional1 = populacao1 / area1;//not ok
-  pibperCapita1 = pib1 / populacao1;//not ok
-
-  densidadePopulacional2 = populacao2 / area2;//not ok
-  pibperCapita1 = pib2 / populacao2;//not ok
-
   //espaço entre entrada de dados
   printf("\n");
   //espaço entre entrada de dados
@@ -86,6 +75,18 @@ int main() {
   printf("Quantos pontos turísticos a cidade possui: ");
   scanf("%d", &pontosTuristicos2);
 
+  // Área para calculo de dados
+
+  pib1 = pib1 * 1000000000;
+  pib2 = pib2 * 1000000000;
+
+  densidadePopulacional1 = populacao1 / area1;
+  pibperCapita1 = pib1 / populacao1;
+
+  densidadePopulacional2 = populacao2 / area2;
+  pibperCapita2 = pib2 / populacao2;
+
+
   // Área para exibição dos dados da cidade
 
   printf("=============================\n");
@@ -100,8 +101,8 @@ int main() {
   printf("Área (em km²): %.2f km²\n", area1);
   printf("PIB: %.2f bilhões de reais\n", pib1);
   printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
-  printf("Dencidade Populacional: %.2f hab/km²\n",   densidadePopulacional1);//not ok
-  printf("PIB per capita: %.2f reais\n", pibperCapita1);//not ok
+  printf("Dencidade Populacional: %.2f hab/km²\n",   densidadePopulacional1);
+  printf("PIB per capita: %.2f reais\n", pibperCapita1);
   printf("\n");
 
   printf("=============================\n");
@@ -115,8 +116,8 @@ int main() {
   printf("Área (em km²): %.2f km²\n", area2);
   printf("PIB: %.2f bilhões de reais\n", pib2);
   printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
-  printf("Dencidade Populacional: %.2f hab/km²\n", densidadePopulacional2);//not ok
-  printf("PIB per capita: %.2f reais\n", pibperCapita2);//not ok
+  printf("Dencidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
+  printf("PIB per capita: %.2f reais\n", pibperCapita2);
   printf("\n");
 
   printf("=============================\n");
